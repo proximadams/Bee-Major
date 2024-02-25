6ea90b55cdeb0ed09db8c0b791548ec1253c1493
Lucky Adams <proximadams@gmail.com>
2024-02-25 16:56:21 -0600

    change default scene

--- Scripts/Pause.gd
+++ Scripts/Pause.gd
@@ -17,26 +17,5 @@ func _input(event):
 			toggle_pause()
 
 func toggle_pause():
-	# Toggle the value of is_game_paused variable
-	is_game_paused = !is_game_paused
-
-	# Checking if the game is paused
-	if is_game_paused:
-		# Loading the pause menu scene if it is not already loaded
-		if not pause_menu_instance:
-
- ##### NOTE: Do not forget to add actual pause screen scene once created #####           
-			pause_menu_instance = load("").instance()
-			add_child(pause_menu_instance)
-		# Pausing the game by setting the tree's paused property to true
-		get_tree().paused = true
-		# Show pause menu
-		pause_menu_instance.show()
-	else:
-		# Unpausing the game by setting the tree's paused property to false
-		get_tree().paused = false
-		# Hide and remove pause menu instance
-		if pause_menu_instance:
-			pause_menu_instance.hide()
-			pause_menu_instance.queue_free()
-			pause_menu_instance = null
+	PauseScene.visible = get_tree().paused
+	get_tree().paused = !get_tree().paused
