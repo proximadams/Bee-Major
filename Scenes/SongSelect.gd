b6dcad5e4544ce6891c49b3d283188f2d78b3b9c
Lucky Adams <proximadams@gmail.com>
2024-02-25 13:41:53 -0600

    song select scene started

---
+++ Scenes/SongSelect.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/EasyButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
